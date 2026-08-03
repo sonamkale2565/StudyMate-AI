3e3343fd6223f51518150befb14755144cb2e8a6
Sonam Kale <sonamkale2565@gmail.com>
2026-08-03 15:00:51 +0530

    Add Hero component for study management

---
+++ src/components/Hero.jsx
@@ -0,0 +1,30 @@
+function Hero(){
+
+  return(
+
+    <section className="hero">
+
+      <div className="hero-content">
+
+        <h1>
+          Study Smarter With StudyMate AI 🚀
+        </h1>
+
+        <p>
+          Manage your study tasks, track progress,
+          and improve productivity with AI powered tools.
+        </p>
+
+        <button>
+          Start Learning
+        </button>
+
+      </div>
+
+    </section>
+
+  );
+
+}
+
+export default Hero;
