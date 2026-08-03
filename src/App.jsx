3269819cdd2a85b587485e4f1da9d8cca77c01c5
Sonam Kale <sonamkale2565@gmail.com>
2026-08-03 15:10:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Dashboard from "./components/Dashboard";
+import AITools from "./components/AITools";
 
-function App() {
+function App(){
 
-  return (
+  return(
+
     <div>
+
       <Navbar />
+
       <Hero />
+
       <Dashboard />
+
+      <AITools />
+
     </div>
+
   );
+
 }
 
 export default App;
